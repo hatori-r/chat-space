30597074e57f5d2fc1ca6cd5e3597b5547dddea6
hatori-r <aruue368@gmail.com>
2020-05-19 19:26:56 +0900

    generated files by db

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,47 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# chat-space データベース設計
+## usersテーブル
+|Column|Type|Options|
+|------|----|-------|
+|id|integer|null: false|
+|name|string|null: false|
+|email|string|null: false|
+|password|string|null: false|
+### Association
+- has_many :chats
+- has_many :comments
+
+## chatsテーブル
+|Column|Type|Options|
+|------|----|-------|
+|id|integer|null: false|
+|title|text|null: false|
+|user_id|integer|null: false, foreign_key: true|
+### Association
+- belongs_to :user
+- has_many :comments
+
+## commentsテーブル
+|Column|Type|Options|
+|------|----|-------|
+|id|integer|null: false|
+|text|text|null: false|
+|user_id|integer|null: false, foreign_key: true|
+|chat_id|integer|null: false, foreign_key: true|
+### Association
+- belongs_to :chat
+- belongs_to :user
+
+## chats-usersテーブル
+|Column|Type|Options|
+|------|----|-------|
+|id|integer|null: false|
+|user_id|integer|null: false, foreign_key: true|
+|chat_id|integer|null: false, foreign_key: true|
+
+### Association
+- belongs_to :chat
+- belongs_to :user
