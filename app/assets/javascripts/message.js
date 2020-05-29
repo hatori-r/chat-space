4974e2a71ad023cadcc8fc04e3e16a1f76f1e41c
hatori-r <aruue368@gmail.com>
2020-05-29 15:26:14 +0900

    master_update

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -3,7 +3,7 @@ $(function(){
   function buildHTML(message){
     if ( message.image ) {
       var html =
-      `<div class="message-list" data-message-id=${message.id}>
+      `<div class="message-list-list" data-message-id=${message.id}>
         <div class="message__upper-info">
           <div class="message__upper-info__talker">
             ${message.user_name}
@@ -67,7 +67,8 @@ $(function(){
   });
 
   var reloadMessages = function() {
-    var last_message_id = $('.message:last').data("message-id");
+    var last_message_id = $('.message-list-list:last').data("message-id");
+    console.log(last_message_id);
     $.ajax({
       url: "api/messages",
       type: 'get',
@@ -75,15 +76,21 @@ $(function(){
       data: {id: last_message_id}
     })
     .done(function(messages) {
+      if (messages.length !== 0) {
       var insertHTML = '';
       $.each(messages, function(i, message) {
         insertHTML += buildHTML(message)
       });
-      $('.messages').append(insertHTML);
-    })
+      $('.message-list').append(insertHTML);
+      $('.message-list').animate({ scrollTop: $('.message-list')[0].scrollHeight});
+    }
+  })
     .fail(function() {
       alert('error');
     });
   };
-  setInterval(reloadMessages, 7000);
+  if (document.location.href.match(/\/groups\/\d+\/messages/)) {
+    setInterval(reloadMessages, 7000);
+  }
+  
 });
